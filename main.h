cbc7132d01f478d5808c74f8ede2aa630d84e012
yousrakdc <yousrakdc@gmail.com>
2024-03-25 14:44:54 +0000

    main

--- main.h
+++ main.h
@@ -2,5 +2,9 @@
 #define MAIN_H
 
 int _printf(const char *format, ...);
+int (*print_func(const char *format))(va_list);
+int print_char(va_list args);
+int print_str(va_list args);
+int print_pct(va_list args);
 
 #endif
