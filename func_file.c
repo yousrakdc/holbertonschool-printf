8e05119727bcf29377bc3644c5d88c0991ce75e6
yousrakdc <yousrakdc@gmail.com>
2024-03-26 11:13:29 +0000

    functions

--- func_file.c
+++ func_file.c
@@ -45,3 +45,48 @@ int print_pct(va_list args)
 	(void)args;
 	return (write(1, "%", 1));
 }
+
+/**
+ * print_dec - prints decimal
+ * @args: arguments
+ * Return: success
+ */
+
+int print_dec(va_list args)
+{
+
+	int n = va_arg(args, int);
+	int num, last = n % 10, digit, exponent = 1;
+	int i = 1;
+
+	n = n / 10;
+	num = n;
+
+	if (last < 0)
+	{
+		putchar('-');
+		num = -num;
+		n = -n;
+		last = -last;
+		i++;
+	}
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exponent = exponent * 10;
+			num = num / 10;
+		}
+		num = n;
+		while (exponent > 0)
+		{
+			digit = num / exponent;
+			putchar(digit + '0');
+			num = num - (digit * exponent);
+			exponent = exponent / 10;
+			i++;
+		}
+	}
+	putchar(last + '0');
+	return (i);
+}
