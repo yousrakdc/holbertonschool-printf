56a611aca04493776e0394be9d787a57f3f7694b
yousrakdc <yousrakdc@gmail.com>
2024-03-29 13:32:21 +0000

    modified

--- _printf.c
+++ _printf.c
@@ -13,7 +13,7 @@ int _printf(const char *format, ...)
 	va_list args;
 
 	va_start(args, format);
-	if (format == NULL || !format[i + 1] == '\0')
+	if (format == NULL || !format[i + 1])
 		return (-1);
 	while (format[i])
 	{
